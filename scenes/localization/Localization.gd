3a87609829b752ebe3c1045816f5e67d46c437d6
Eugen Paul <paul.eugen@gmx.de>
2024-11-01 09:57:21 +0100

    Edit localization.

--- scenes/localization/Localization.gd
+++ scenes/localization/Localization.gd
@@ -1,14 +1,14 @@
 extends Node
 
 func FUNC_GET_X_POINTS(n: int) -> String:
-    return tr_n("Get at least {0} point.", "Get at least {0} points.", n)
+    return tr_n("GET_X_POINTS.", "GET_X_POINTS_PLURAL.", n)
 
 func FUNC_CATCH_X_ROCKETS(n: int) -> String:
-    return tr_n("Catch at least {0} rocket.","Catch at least {0} rockets.",n)
+    return tr_n("CATCH_X_ROCKETS.","CATCH_X_ROCKETS_PLURAL.",n)
     
 func FUNC_CATCH_X_BOMBS(n: int) -> String:
-    return tr_n("Catch at least {0} bomb.","Catch at least {0} bombs.",n)
+    return tr_n("CATCH_X_BOMBS.","CATCH_X_BOMBS_PLURAL.",n)
     
-var DONT_USE_ROCKETS:String = tr("Do not use rockets.")
-var DONT_LOSE_ANY_LIFE:String = tr("Don't lose any life.")
-var DONT_CATCH_ANY_ITEMS:String = tr("Don't catch any items.")
+var DONT_USE_ROCKETS:String = tr("DONT_USE_ROCKETS.")
+var DONT_LOSE_ANY_LIFE:String = tr("DONT_LOSE_ANY_LIFE.")
+var DONT_CATCH_ANY_ITEMS:String = tr("DONT_CATCH_ANY_ITEMS.")
