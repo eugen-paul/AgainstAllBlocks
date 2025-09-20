8df7f05553a713edd1cce7e0e72a81c1028b1e3f
Eugen Paul <paul.eugen@gmx.de>
2025-09-20 12:07:18 +0200

    Level 15 - The fans cheer.

--- skripts/world/Goal.cs
+++ skripts/world/Goal.cs
@@ -2,6 +2,12 @@ using Godot;
 
 public partial class Goal : Node3D, ITriggerable
 {
+
+    public override void _Ready()
+    {
+        GetNode<AudioStreamPlayer>("AudioStreamPlayer").Finished += StopAnimation;
+    }
+
     public void Trigger()
     {
         var blocks = FindChildren("Block*");
@@ -13,8 +19,58 @@ public partial class Goal : Node3D, ITriggerable
         var blockNummer = blockName[(blockName.Length - 1)..];
         var explosion = GetNode<GpuParticles3D>("Fire" + blockNummer);
         explosion.Emitting = true;
-        
+
         GetNode<AudioStreamPlayer>("AudioStreamPlayer").Play();
+
+        StartAnimation();
+    }
+
+    private void StartAnimation()
+    {
+        var bg = GetNodeOrNull<Node>("/root/Level15/Background");
+        if (bg == null)
+        {
+            return;
+        }
+        var children = bg.GetChildren();
+        foreach (var child in children)
+        {
+            if (child is Node3D)
+            {
+                var cc = child.GetChildren();
+                foreach (var c in cc)
+                {
+                    if (c is AnimatedSprite3D ap)
+                    {
+                        ap.Play("Goal");
+                    }
+                }
+            }
+        }
+    }
+
+    private void StopAnimation()
+    {
+        var bg = GetNodeOrNull<Node>("/root/Level15/Background");
+        if (bg == null)
+        {
+            return;
+        }
+        var children = bg.GetChildren();
+        foreach (var child in children)
+        {
+            if (child is Node3D)
+            {
+                var cc = child.GetChildren();
+                foreach (var c in cc)
+                {
+                    if (c is AnimatedSprite3D ap)
+                    {
+                        ap.Play("Idle");
+                    }
+                }
+            }
+        }
     }
 
 }
