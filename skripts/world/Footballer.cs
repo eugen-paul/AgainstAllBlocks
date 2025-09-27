9d3c440b6d44f1e309983ab8e0f1e4b1d10e0902
Eugen Paul <paul.eugen@gmx.de>
2025-09-27 14:25:02 +0200

    Flip footballer by moving

--- skripts/world/Footballer.cs
+++ skripts/world/Footballer.cs
@@ -3,6 +3,7 @@ using Godot;
 public partial class Footballer : Path3D
 {
     private const string PATHFOLLOW3D_CHILD_NAME = "PathFollow3D";
+    private const string Sprite3D_CHILD_NAME = "PathFollow3D/Footballer/Sprite3D";
 
     [Export]
     private float Speed { get; set; } = 1.0f;
@@ -24,6 +25,11 @@ public partial class Footballer : Path3D
         if (GetNodeOrNull<PathFollow3D>(PATHFOLLOW3D_CHILD_NAME) is PathFollow3D pathFollow)
         {
             pathFollow.Progress += (float)delta * Speed;
+
+            if (GetNodeOrNull<Sprite3D>(Sprite3D_CHILD_NAME) is Sprite3D sprite)
+            {
+                sprite.FlipH = pathFollow.ProgressRatio > .5f;
+            }
         }
     }
 
